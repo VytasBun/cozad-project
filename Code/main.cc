d30149cd022dd893acd0ad071eeaba9f64c20218
VytasBun <129725021+VytasBun@users.noreply.github.com>
2024-01-23 03:48:30 +0000

    double int

--- Code/main.cc
+++ Code/main.cc
@@ -1,11 +1,16 @@
 #include <iostream>
 
+// preferred version
+int main()
+{
+	std::cout << "Enter an integer: ";
 
-int main(){
-    std::string str = "Hell Word!";
-    std::cout << str << std::endl;
-    
-    return 0;
+	int num{ };
+	std::cin >> num;
+
+	std::cout << "Double that number is: " <<  num * 2 << '\n'; // use an expression to multiply num * 2 at the point where we are going to print it
+
+	return 0;
 }
 
 //ballsack//
